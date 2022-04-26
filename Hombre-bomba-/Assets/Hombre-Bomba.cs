66320c5ae1711813862d1890b89757d71cdb391c
ANG <chikkoy99@gmail.com>
2022-04-26 16:06:12 -0300

    Create Hombre-Bomba.cs

---
+++ Hombre-bomba-/Assets/Hombre-Bomba.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    bool Vida;
+    int Speed;
+
+    void Caminar() {
+                    }
+    void Bomba() { 
+    }
+    void Daño () {
+        }
+
+
+
+}
